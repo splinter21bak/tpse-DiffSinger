25dbb3c172417e6abdb9df84a332dd24a54eeff6
KakaruHayate <kakaru.hk@hotmail.com>
2024-11-01 16:11:06 +0800

    ConvFFT

--- modules/commons/common_layers.py
+++ modules/commons/common_layers.py
@@ -9,6 +9,7 @@ from torch import nn
 from torch.nn import LayerNorm, MultiheadAttention, ReLU, GELU, SiLU
 
 import utils
+from utils.hparams import hparams
 
 
 class NormalInitEmbedding(torch.nn.Embedding):
@@ -143,6 +144,24 @@ class EncSALayer(nn.Module):
         self.ffn = TransformerFFNLayer(
             c, 4 * c, kernel_size=kernel_size, dropout=relu_dropout, act=act
         )
+        
+        # ConvFFT args:
+        self.use_conv_block = hparams['use_conv_block']
+        self.conv_block_kernel_size = hparams['conv_block_kernel_size']
+        self.conv_block_dropout_rate = hparams['conv_block_dropout_rate']
+        self.conv_block_dilate = hparams['conv_block_dilate']
+        self.conv_block_layer = hparams['conv_block_layer']
+        if self.use_conv_block:
+            self.conv_blocks = torch.nn.ModuleList([
+                Conv_Block(
+                    channels= c,
+                    kernel_size= self.conv_block_kernel_size,
+                    dropout_rate= self.conv_block_dropout_rate, 
+                    dilate = self.conv_block_dilate
+                    )
+                for _ in range(self.conv_block_layer)
+                ])
+                
 
     def forward(self, x, encoder_padding_mask=None, **kwargs):
         layer_norm_training = kwargs.get('layer_norm_training', None)
@@ -150,21 +169,88 @@ class EncSALayer(nn.Module):
             self.layer_norm1.training = layer_norm_training
             self.layer_norm2.training = layer_norm_training
         residual = x
-        x = self.layer_norm1(x)
-        x, _, = self.self_attn(
-            query=x,
-            key=x,
-            value=x,
+        x_aten = self.layer_norm1(x)
+        x_aten, _, = self.self_attn(
+            query=x_aten,
+            key=x_aten,
+            value=x_aten,
             key_padding_mask=encoder_padding_mask
         )
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = residual + x
-        x = x * (1 - encoder_padding_mask.float()).transpose(0, 1)[..., None]
+        x_aten = F.dropout(x_aten, self.dropout, training=self.training)
 
+        if self.use_conv_block:
+            x_convs = 0
+            for block in self.conv_blocks:
+                x_conv = block(x)
+                x_convs = x_convs + x_conv
+            x = residual + x_convs + x_aten
+        else:
+            x = residual + x_aten
+        x = x * (1 - encoder_padding_mask.float()).transpose(0, 1)[..., None]
+        
         residual = x
         x = self.layer_norm2(x)
         x = self.ffn(x)
         x = F.dropout(x, self.dropout, training=self.training)
         x = residual + x
         x = x * (1 - encoder_padding_mask.float()).transpose(0, 1)[..., None]
+        return x
+
+
+class Conv1d(torch.nn.Conv1d):
+    def __init__(self, w_init_gain= 'linear', *args, **kwargs):
+        self.w_init_gain = w_init_gain
+        super().__init__(*args, **kwargs)
+
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.weight, gain= torch.nn.init.calculate_gain(self.w_init_gain))
+        if not self.bias is None:
+            torch.nn.init.zeros_(self.bias)
+
+
+class Conv_Block(torch.nn.Module):
+    def __init__(
+        self,
+        channels: int,
+        kernel_size: int=5,
+        dropout_rate: float= 0.1, 
+        dilate:int = 4
+        ) -> None:
+        super().__init__()
+
+        self.conv_0 = Conv1d(
+            in_channels= channels,
+            out_channels= channels * dilate,
+            kernel_size= kernel_size,
+            padding= (kernel_size - 1) // 2,
+            w_init_gain= 'linear'
+            )
+        self.norm_0 = LayerNorm(channels * dilate)
+        self.relu = torch.nn.ReLU()
+        self.dropout = torch.nn.Dropout(p= dropout_rate)
+        self.conv_1 = Conv1d(
+            in_channels= channels * dilate,
+            out_channels= channels,
+            kernel_size= kernel_size,
+            padding= (kernel_size - 1) // 2,
+            w_init_gain= 'linear'
+            )
+        self.norm_1 = LayerNorm(channels)
+
+    def forward(
+        self,
+        x: torch.Tensor
+        ) -> torch.Tensor:
+        '''
+        # x: T x B x C
+        '''
+        residuals = x
+
+        x = self.conv_0(x.permute(1, 2, 0)).permute(2, 0, 1)
+        x = self.norm_0(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.conv_1(x.permute(1, 2, 0)).permute(2, 0, 1)
+        x = self.norm_1(x + residuals)
+        
         return x
